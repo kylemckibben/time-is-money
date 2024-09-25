43c946a7450c98d9516b5605911467b6aeba5cfd
kylemckibben <kylemckibben@proton.me>
2024-09-25 14:12:34 -0400

    Change slash command to be strict match

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def on_message(message):
         """Ignore messages from the bot."""
         return
 
-    if message.content.startswith('/generate-class-emojis'):
+    if message.content == '/generate-class-emojis':
         """Generate class emojis on server."""
         class_icons = generate_class_icon_byteobjs()
         for i in range(len(class_names)):
